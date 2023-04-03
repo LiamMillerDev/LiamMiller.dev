fb670dd5965cc1bb503f01004d51ac7ae650c25f
LiamMillerNZ <LiamMillerNZ@outlook.com>
2023-04-03 20:25:27 +1200

    added header background when scrolling

---
+++ js/main.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get DOM elements
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+  const socialLinks = document.querySelector(".social-links");
+
+  // Toggle navigation menu
+  function toggleMenu() {
+    if (navLinks.classList.contains("active")) {
+      navLinks.classList.remove("show");
+      setTimeout(() => {
+        navLinks.classList.remove("active");
+        document.querySelector(".navbar").appendChild(socialLinks);
+      }, 300);
+    } else {
+      navLinks.classList.add("active");
+      navLinks.appendChild(socialLinks);
+      setTimeout(() => {
+        navLinks.classList.add("show");
+      }, 50);
+    }
+    socialLinks.classList.toggle("active");
+  }
+
+  // Attach event listeners
+  hamburger.addEventListener("click", toggleMenu);
+  window.addEventListener("resize", closeMenuOnResize);
+
+  // Close navigation menu on window resize
+  function closeMenuOnResize() {
+    if (window.innerWidth >= 769) {
+      navLinks.classList.remove("active");
+      navLinks.classList.remove("show");
+      socialLinks.classList.remove("active");
+      document.querySelector(".navbar").appendChild(socialLinks);
+    }
+  }
+});
+
+// Select the navbar and parallax elements
+const navbar = document.querySelector(".navbar");
+const parallax = document.querySelector(".parallax");
+
+function toggleNavbarBackground() {
+  let viewportHeight = window.innerHeight;
+  let scrollPosition = parallax.scrollTop;
+  let opacity = Math.min(scrollPosition / viewportHeight, 1);
+
+  navbar.style.backgroundColor = `rgba(233, 192, 217, ${opacity})`;
+}
+
+// Add event listener for scroll events
+parallax.addEventListener("scroll", toggleNavbarBackground);
+
+window.onload = function () {
+  const loadingOverlay = document.getElementById("loading-overlay");
+  const loadingLogo = document.getElementById("loading-logo");
+
+  setTimeout(() => {
+    loadingLogo.style.opacity = "0";
+  }, 500);
+
+  setTimeout(() => {
+    loadingOverlay.style.transform = "translateY(-100%)";
+  }, 1000);
+};
