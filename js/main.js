dfac20222540510acdf31b0ef2312647ca83926d
Liam Miller <LiamJmillerNZ@gmail.com>
2023-04-16 20:58:44 +1200

    added animation

--- js/main.js
+++ js/main.js
@@ -1,85 +1,78 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Get DOM elements
-
-  const socialLinks = document.querySelector(".social-links");
-
-  // Toggle navigation menu
-  function toggleMenu() {
-    hamburger.classList.toggle("active");
-    if (navLinks.classList.contains("active")) {
-      navLinks.classList.remove("show");
-      setTimeout(() => {
-        navLinks.classList.remove("active");
-        document.querySelector(".navbar").appendChild(socialLinks);
-      }, 300);
-    } else {
-      navLinks.classList.add("active");
-      navLinks.appendChild(socialLinks);
-      setTimeout(() => {
-        navLinks.classList.add("show");
-      }, 50);
-    }
-    socialLinks.classList.toggle("active");
-  }
-
-  // Attach event listeners
-  hamburger.addEventListener("click", toggleMenu);
-  window.addEventListener("resize", closeMenuOnResize);
-
-  // Close navigation menu and reset hamburger icon on window resize
-  function closeMenuOnResize() {
-    if (window.innerWidth >= 769) {
-      navLinks.classList.remove("active");
-      navLinks.classList.remove("show");
-      socialLinks.classList.remove("active");
-      document.querySelector(".navbar").appendChild(socialLinks);
-      hamburger.classList.remove("active"); // Reset hamburger icon
-    }
-  }
-});
-
-// Select the navbar and parallax elements
 const navbar = document.querySelector(".navbar");
 const parallax = document.querySelector(".parallax");
 
-function toggleNavbarBackground() {
-  let viewportHeight = window.innerHeight;
-  let scrollPosition = parallax.scrollTop;
-  let opacity = Math.min(scrollPosition / viewportHeight, 1);
+document.addEventListener("DOMContentLoaded", function () {
+  const socialLinks = document.querySelector(".social-links");
 
-  navbar.style.backgroundColor = `rgba(255,255,255, ${opacity})`;
+  hamburger.addEventListener("click", function () {
+    toggleMenu(socialLinks);
+  });
+
+  window.addEventListener("resize", function () {
+    handleResize(socialLinks);
+  });
+});
+
+function toggleMenu(socialLinks) {
+  hamburger.classList.toggle("active");
+
+  if (navLinks.classList.contains("active")) {
+    navLinks.classList.remove("show");
+
+    setTimeout(() => {
+      navLinks.classList.remove("active");
+      document.querySelector(".navbar").appendChild(socialLinks);
+    }, 300);
+  } else {
+    navLinks.classList.add("active");
+    navLinks.appendChild(socialLinks);
+
+    setTimeout(() => {
+      navLinks.classList.add("show");
+    }, 50);
+  }
+
+  socialLinks.classList.toggle("active");
 }
 
-// Add event listener for scroll events
-parallax.addEventListener("scroll", toggleNavbarBackground);
+function handleResize(socialLinks) {
+  if (window.innerWidth >= 769) {
+    navLinks.classList.remove("active");
+    navLinks.classList.remove("show");
+    socialLinks.classList.remove("active");
+    document.querySelector(".navbar").appendChild(socialLinks);
+    hamburger.classList.remove("active");
+  }
+}
 
-window.onload = function () {
-  const loadingOverlay = document.getElementById("loading-overlay");
-  const loadingLogo = document.getElementById("loading-logo");
-
-  setTimeout(() => {
-    loadingLogo.style.opacity = "0";
-  }, 500);
-
-  setTimeout(() => {
-    loadingOverlay.style.transform = "translateY(-100%)";
-  }, 1000);
-};
+function toggleNavbarBackground() {
+  const windowHeight = window.innerHeight;
+  const scrollTop = parallax.scrollTop;
+  navbar.style.backgroundColor = `rgba(255, 255, 255, ${Math.min(
+    scrollTop / windowHeight,
+    1
+  )})`;
+}
 
 function updateHamburger() {
   if (window.innerWidth <= 768) {
-    if (navLinks.classList.contains("show")) {
-      hamburger.classList.add("active");
-    } else {
-      hamburger.classList.remove("active");
-    }
+    navLinks.classList.contains("show")
+      ? hamburger.classList.add("active")
+      : hamburger.classList.remove("active");
   } else {
     hamburger.classList.remove("active");
     navLinks.classList.remove("show");
   }
 }
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    duration: 1000,
+    easing: "ease",
+    once: true,
+  });
+});
 
+parallax.addEventListener("scroll", toggleNavbarBackground);
 window.addEventListener("resize", updateHamburger);
